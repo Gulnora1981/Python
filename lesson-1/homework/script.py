22590173c75a1d7afc573b2470b01a7181988c73
Gulnora1981 <guulnora@gmail.com>
2025-08-12 20:12:40 +0500

    Create script.py

---
+++ lesson-1/homework/script.py
@@ -0,0 +1,21 @@
+## Homework 1
+a = 5
+p = a*4
+print(p)
+
+s = a**2
+print(s)
+d = 15
+i = 3.14159*d
+print(i)
+a = 20
+b = 30
+
+c = (a+b)/2
+print(c)
+d = a + b
+print(d)
+e = a * b
+print(e)
+s = a**2 + b**2
+print(s)
